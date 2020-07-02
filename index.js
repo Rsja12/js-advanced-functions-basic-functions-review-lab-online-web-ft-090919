34060330f8020bca8094f821761b537dfa9b17a1
Alfonso Socarras <asocarras10@gmail.com>
2020-07-02 00:39:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function saturdayFun(activity = 'roller-skate') {
 function mondayWork(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`
 }
+
+Calculator = {
+  
+  add: function(a, b) {
+    return a + b
+  }
+  
+}
